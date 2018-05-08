e173da89a38dc82fbd4ec0cd3fac76cc5ae90e04
asif eqbal <asif.eqbal@vrisini.com>
2018-05-08 10:30:52 +0530

    addd

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "phonegap-plugin-push.PushNotification",
+    "file": "plugins/phonegap-plugin-push/www/push.js",
+    "pluginId": "phonegap-plugin-push",
+    "clobbers": [
+      "PushNotification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "phonegap-plugin-push": "1.8.4",
+  "cordova-plugin-splashscreen": "5.0.2"
+};
+// BOTTOM OF METADATA
+});
