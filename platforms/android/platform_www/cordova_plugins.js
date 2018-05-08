155ffe0413ff5a7b241b458aa44dea1336cf48a7
asif eqbal <asif.eqbal@vrisini.com>
2018-05-08 11:50:17 +0530

    hhhmmm

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -21,7 +21,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "phonegap-plugin-push": "1.8.4",
-  "cordova-plugin-splashscreen": "4.1.0"
+  "cordova-plugin-splashscreen": "4.1.0",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
